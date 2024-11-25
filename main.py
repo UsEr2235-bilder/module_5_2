faa016a7b44e295411eeffb5f6fe79e720010fe7
UsEr2235-bilder <anonym.mirniy.98@gmail.com>
2024-11-25 18:21:32 +0300

    main.py

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ class House:
     def __str__(self):
         return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
 
-# Пример выполняемого кода:
+
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 
-# __str__
-print(h1)  # Название: ЖК Эльбрус, кол-во этажей: 10
-print(h2)  # Название: ЖК Акация, кол-во этажей: 20
 
-# __len__
+print(h1)  
+print(h2)  
+
+
 print(len(h1))  # 10
 print(len(h2))  # 20
